01d58979cb46357d8e50550d32eada2cf13986b1
amiyo-mondol-sazim <amiyo.mondol@sazim.io>
2026-01-12 15:12:42 +0600

    chore: add jsdom as a dependency and update package-lock.json

--- common/api/brand/brand.keys.ts
+++
@@ -1,5 +0,0 @@
-export const brandQueryKeys = {
-  all: ['brands'],
-  lists: () => [...brandQueryKeys.all, 'list'],
-  list: (params: Record<string, unknown>) => [...brandQueryKeys.lists(), params],
-};
--- common/api/tenant/tenant.keys.ts
+++
@@ -1,7 +0,0 @@
-import type { TTenantsQueryParams } from '~/common/typedefs/api';
-
-export const tenantQueryKeys = {
-  all: ['banks'],
-  lists: () => [...tenantQueryKeys.all, 'list'],
-  list: (params: TTenantsQueryParams) => [...tenantQueryKeys.lists(), params],
-};
