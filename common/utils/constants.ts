3349df0ebd3721eeb165162d61097433cbd98cb0
amiyo-mondol-sazim <amiyo.mondol@sazim.io>
2026-01-12 14:15:48 +0600

    chore: remove new offer button and cleanup constants

--- common/utils/constants.ts
+++ common/utils/constants.ts
@@ -1,13 +1,10 @@
 export const PAGE_URLS = {
-  HOME: '/',
-  LOGIN: '/auth/login',
-  NEW_OFFER: '/offers/create',
-  DRAFT_OFFER: (id: number) => `/offers/create?offerId=${id}`,
-  OFFER: (id: number) => `/offers/${id}`,
+  HOME: "/",
+  LOGIN: "/auth/login",
 };
 
-export const DEFAULT_OFFER_IMAGE_URL = '/images/offer-placeholder.svg';
+export const DEFAULT_OFFER_IMAGE_URL = "/images/offer-placeholder.svg";
 
 export const MEGABYTE = 1024 * 1024;
 
-export const ACCESS_TOKEN_STORAGE_KEY = 'access_token';
+export const ACCESS_TOKEN_STORAGE_KEY = "access_token";
