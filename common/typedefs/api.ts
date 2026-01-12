579efea43cf6a041ddb852c4a1b15c3424e4ee8c
amiyo-mondol-sazim <amiyo.mondol@sazim.io>
2026-01-12 14:27:59 +0600

    refactor(types): remove unused merchant api types

--- common/typedefs/api.ts
+++ common/typedefs/api.ts
@@ -1,23 +1,6 @@
-import type { components, paths } from './api-schema';
+import type { components } from "./api-schema";
 
-export type TUser = components['schemas']['MerchantUser'];
-export type TOffer = components['schemas']['OfferList'];
-export type TOfferAttachment = components['schemas']['OfferAttachment'];
-export type TOfferStatus = TOffer['status'];
-export type TOfferQueryStatus = NonNullable<
-  NonNullable<paths['/api/v1/merchant/offers']['get']['parameters']['query']>['status']
->;
-export type TOfferDetails = components['schemas']['Offer'];
-export type TOfferRule = NonNullable<components['schemas']['OfferRule']>;
-export type TCreateOffer = components['schemas']['CreateOffer'];
-export type TUpdateOffer = paths['/api/v1/merchant/offers/{id}']['put']['requestBody']['content']['application/json'];
-export type TTenantsQueryParams = NonNullable<paths['/api/v1/merchant/tenants']['get']['parameters']['query']>;
-
-export type TRuleTargetUserCountPayload =
-  paths['/api/v1/merchant/rule_target_count']['post']['requestBody']['content']['application/json'];
-export type TCashbackRuleConfig = components['schemas']['CashbackRuleConfig'];
-export type TFixedCashbackConfig = Extract<TCashbackRuleConfig, { cashback_amount: number }>;
-export type TPercentageCashbackConfig = Extract<TCashbackRuleConfig, { cashback_percentage: number }>;
+export type TUser = components["schemas"]["MerchantUser"];
 
 /**
  * Legacy Mock API types
