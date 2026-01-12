683470acde141597f54ad76b8d77a7837d17c389
amiyo-mondol-sazim <amiyo.mondol@sazim.io>
2026-01-12 14:29:31 +0600

    fix(types): remove offer dependencies from utils and components

--- common/components/DndImageUpload/DndImageUpload.types.ts
+++ common/components/DndImageUpload/DndImageUpload.types.ts
@@ -1,9 +1,6 @@
-import type { TOfferAttachment } from '~/common/typedefs/api';
-import type { ACCEPTED_FILE_TYPES } from './DndImageUpload.constants';
-
 export type TDndImageUploadProps = {
   accept?: Array<(typeof ACCEPTED_FILE_TYPES)[number] | (string & {})>;
-  modelValue: File | TOfferAttachment | null;
+  modelValue: File | { id: number; url?: string; image_url?: string } | null;
   maxFileSize?: number;
   maxDimensions?: { width: number; height: number };
 };
